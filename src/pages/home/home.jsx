621b8e281df0e0176a34915e38d018314427c7d6
Ronel028 <floridaronel15@gmail.com>
2023-01-26 12:58:37 +0800

    add checkboxes and radio button

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -13,6 +13,54 @@ const Home = () => {
           <Search />
         </div>
       </header>
+      <section className="job-list-section">
+        <main className="job-list-container">
+          <div className="job-input-container">
+            <label htmlFor="fullTime" className="full-time">
+              <input type="checkbox" name="fullTime" id="fullTime" />
+              <div className="checkmark">
+                <div className="check-bg">
+                  <span class="material-icons">check</span>
+                </div>
+              </div>
+              Full time
+            </label>
+            <div className="search-location">
+              <h2>LOCATION</h2>
+              <div>
+                <div className="location-input-container">
+                  <span class="material-icons">public</span>
+                  <input
+                    type="text"
+                    name="searchLocation"
+                    id="searchLocation"
+                    placeholder="City, state, zip code or country"
+                  />
+                </div>
+                <div className="place">
+                  <div className="london">
+                    <input type="radio" name="place" id="london" />
+                    <label htmlFor="london">London</label>
+                  </div>
+                  <div className="amsterdam">
+                    <input type="radio" name="place" id="amsterdam" />
+                    <label htmlFor="amsterdam">Amsterdam</label>
+                  </div>
+                  <div className="new-york">
+                    <input type="radio" name="place" id="new-york" />
+                    <label htmlFor="new-york">New York</label>
+                  </div>
+                  <div className="berlin">
+                    <input type="radio" name="place" id="berlin" />
+                    <label htmlFor="berlin">Berlin</label>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+          <div></div>
+        </main>
+      </section>
     </>
   );
 };
